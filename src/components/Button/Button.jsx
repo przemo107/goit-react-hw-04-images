f95556a2d93f77d2a65b0432c2f556bf9b93577a
Przemysław Soliński <solinskip107@gmail.com>
2023-06-28 20:03:26 +0200

    working but problem wih modal

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,11 @@
-import { Component } from 'react';
 import css from './Button.module.css';
 
-class Button extends Component {
-  render() {
-    const { handleMoreImage } = this.props;
-    return (
-      <>
-        <button className={css.button} onClick={handleMoreImage}>
-          Load More
-        </button>
-      </>
-    );
-  }
-}
-
-export default Button;
+export const Button = ({ handleMoreImage }) => {
+  return (
+    <>
+      <button className={css.button} onClick={handleMoreImage}>
+        Load More
+      </button>
+    </>
+  );
+};
